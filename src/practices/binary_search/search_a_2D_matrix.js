0adcc0c71f2fa2d4fdc4bff760ea61bebbc15943
Lumizz <yiansh15@gmail.com>
2024-08-03 00:42:37 +1000

    day5

---
+++ src/practices/binary_search/search_a_2D_matrix.js
@@ -0,0 +1,66 @@
+/**
+
+You are given an m x n integer matrix matrix with the following two properties:
+
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+
+You must write a solution in O(log(m * n)) time complexity.
+ 
+*/
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+  const colLength = matrix[0].length;
+
+  // Find the row
+  let top = 0;
+  let bot = matrix.length - 1;
+  let row = 0;
+
+  while (top <= bot) {
+    const middle = Math.floor((top + bot) / 2);
+
+    if (target > matrix[middle][colLength - 1]) {
+      top = middle + 1;
+    } else if (target < matrix[middle][0]) {
+      bot = middle - 1;
+    } else {
+      row = middle;
+      break;
+    }
+  }
+
+  // Find the index of the row
+
+  let left = 0;
+  let right = colLength - 1;
+
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+
+    if (target === matrix[row][middle]) return true;
+
+    if (target > matrix[row][middle]) left = middle + 1;
+    else right = middle - 1;
+  }
+  return false;
+};
+
+export const main = async () => {
+  console.log(
+    searchMatrix(
+      [
+        [1, 3, 5, 7],
+        [10, 11, 16, 20],
+        [23, 30, 34, 60],
+      ],
+      32
+    )
+  );
+};
---
+++ src/practices/sliding_window/longest_substring_without_repeating_characters.js
@@ -0,0 +1,55 @@
+/**
+
+Given a string s, find the length of the longest substring without repeating characters.
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  let left = 0;
+  let max = 0;
+  let currentSet = new Set();
+
+  for (let right = 0; right < s.length; right++) {
+    // Remove all letter before and include the repeat
+    while (currentSet.has(s[right])) {
+      currentSet.delete(s[left]);
+      left += 1;
+    }
+
+    currentSet.add(s[right]);
+    max = Math.max(max, currentSet.size);
+  }
+
+  return max;
+};
+
+export const main = async () => {
+  console.log(lengthOfLongestSubstring("abcabcbb") === 3);
+  console.log(lengthOfLongestSubstring("bbbbb") === 1);
+  console.log(lengthOfLongestSubstring("pwwkew") === 3);
+  console.log(lengthOfLongestSubstring("au") === 2);
+  console.log(lengthOfLongestSubstring(" ") === 1);
+  console.log(lengthOfLongestSubstring("abcb") === 3);
+  console.log(lengthOfLongestSubstring("abc") === 3);
+};
