d6f23e7439f24e05d95f209494b07ecfcca38e4b
Lumizz <yiansh15@gmail.com>
2025-06-21 22:22:42 +1000

    day63

---
+++ src/practices/math_and_geometry/counting-bits.js
@@ -0,0 +1,38 @@
+/**
+
+[Easy]
+
+338. Counting Bits
+
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+*/
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function (n) {
+  const dp = new Array(n + 1).fill(0);
+
+  const res = [0];
+
+  let offset = 1;
+
+  for (let i = 1; i <= n; i++) {
+    if (offset * 2 === i) {
+      offset = i;
+    }
+
+    dp[i] = 1 + dp[i - offset];
+    res.push(dp[i]);
+  }
+
+  return res;
+};
+
+export const main = async () => {
+  console.log(countBits(4)); // [0,1,1,2,1]
+  console.log(countBits(2)); // [0,1,1]
+  console.log(countBits(5)); // [0,1,1,2,1,2]
+};
---
+++ src/practices/math_and_geometry/number-of-1-bits.js
@@ -0,0 +1,46 @@
+/**
+
+[Easy]
+
+338. Counting Bits
+
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+*/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var hammingWeight_with_bitwise = function (n) {
+  let res = 0;
+
+  while (n > 0) {
+    n = n & (n - 1);
+    res += 1;
+  }
+
+  return res;
+};
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+  let res = 0;
+
+  while (n > 0) {
+    // Add 1 if current n is odds
+    res += n % 2;
+    // Right shift one bit
+    n = n >> 1;
+  }
+
+  return res;
+};
+
+export const main = async () => {
+  console.log(hammingWeight(11)); // 3
+  console.log(hammingWeight(128)); // 1
+};
